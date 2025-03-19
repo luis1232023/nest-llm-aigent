fdf8128bffff16225d6f661cebc81cf5501f22eb
liuliang <liuliang@ychealth.com.cn>
2025-03-19 14:24:33 +0800

    初始化环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { McpModule } from './mcp/mcp.module';
 import { ConfigModule } from '@nestjs/config';
-
+console.log("注入环境变量");
+console.log(process.env.NODE_ENV);
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 设置为全局模块
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`, // 根据环境加载文件
+      envFilePath: `${(process.env.NODE_ENV || '')?'.'+process.env.NODE_ENV:''}.env`, // 根据环境加载文件
     }),
     McpModule
   ],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    // const stdioMcpClientToFunction = StdioMcpClientToFunction.getInstance();
-    // const tools = await stdioMcpClientToFunction.callTool("get_hospital_by_city", {
-    //   city:"上海"
-    // })
-    // const tools = await stdioMcpClientToFunction.getAllMcpServer();
     return JSON.stringify("hello world");
   }
 }
